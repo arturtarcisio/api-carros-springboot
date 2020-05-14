cfbf4942c93e79a7e6015e818dde7c134674b76b
Artur Tarcísio <arturtarcisio1@gmail.com>
2020-05-14 11:05:31 -0300

    Teste para inserir um carro - JUnit

--- src/test/java/com/arturtarcisio/carros/CarrosApplicationTests.java
+++ src/test/java/com/arturtarcisio/carros/CarrosApplicationTests.java
@@ -1,13 +1,29 @@
 package com.arturtarcisio.carros;
 
+import com.arturtarcisio.carros.domain.Carro;
+import com.arturtarcisio.carros.service.CarroService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CarrosApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private CarroService service;
+
+    @Test
+    public void test1() {
+        Carro carro = new Carro();
+        carro.setNome("Ferrari");
+        carro.setTipo("Luxo");
+
+        service.insert(carro);
+     }
+
+     @Test
+     public void test2(){
+
+     }
 
 }
