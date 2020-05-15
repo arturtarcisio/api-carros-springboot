9f8ded0481710577f79fe3f738877816fcbd8094
Artur Tarcísio <arturtarcisio1@gmail.com>
2020-05-15 12:10:52 -0300

    Adicionando Spring Security - Customizando a senha da autenticação e definindo Basic Auth como padrão

--- src/main/java/com/arturtarcisio/carros/exception/ExceptionConfig.java
+++ src/main/java/com/arturtarcisio/carros/exception/ExceptionConfig.java
@@ -4,9 +4,10 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class ExceptionConfig {
+public class ExceptionConfig extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({
             EmptyResultDataAccessException.class
---
+++ src/main/java/com/arturtarcisio/carros/security/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.arturtarcisio.carros.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+            .anyRequest().authenticated().and()
+            .httpBasic();
+    }
+}
